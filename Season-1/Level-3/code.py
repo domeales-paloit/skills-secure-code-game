5da122c7a9a0be508ce738653e1a12e5aeb0ddfa
Dom Eales @ PaloIT <93354893+domeales-paloit@users.noreply.github.com>
2024-07-04 09:53:40 +0000

    S1L3 done

--- Season-1/Level-3/code.py
+++ Season-1/Level-3/code.py
@@ -25,7 +25,7 @@ class TaxPayer:
     def get_prof_picture(self, path=None):
         # setting a profile picture is optional
         if not path:
-            pass
+            return None
 
         # defends against path traversal attacks
         if path.startswith('/') or path.startswith('..'):
@@ -34,6 +34,9 @@ class TaxPayer:
         # builds path
         base_dir = os.path.dirname(os.path.abspath(__file__))
         prof_picture_path = os.path.normpath(os.path.join(base_dir, path))
+
+        if not prof_picture_path.startswith(base_dir):
+            return None
 
         with open(prof_picture_path, 'rb') as pic:
             picture = bytearray(pic.read())
@@ -48,7 +51,18 @@ class TaxPayer:
         if not path:
             raise Exception("Error: Tax form is required for all users")
 
-        with open(path, 'rb') as form:
+        # defends against path traversal attacks
+        if path.startswith('..'):
+            return None
+
+        # builds path
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        tax_form_path = os.path.abspath(path)
+
+        if not tax_form_path.startswith(base_dir):
+            return None
+
+        with open(tax_form_path, 'rb') as form:
             tax_data = bytearray(form.read())
 
         # assume that tax data is returned on screen after this
